859dcbe364cc7341340d55d89841edcd376211d2
carlogilmar <carlogilmar12@gmail.com>
2017-11-27 17:08:32 -0600

    Adding action addEvaluation in route for new eval

--- app/routes/evaluations/new.js
+++ app/routes/evaluations/new.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions:{
+    addEvaluation:function(){
+      var evaluation = this.store.createRecord('evaluation', {
+        title: this.controller.get('title'),
+        author: this.controller.get('author')
+      });
+      evaluation.save().then( () => {
+        alert("Evaluacion Ok");
+      },
+      function(){
+        alert("No se registro la evaluación");
+      });
+    }
+  }
 });
