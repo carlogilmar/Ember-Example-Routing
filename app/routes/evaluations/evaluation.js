bda7d82cad723d669ff27d55eddd8531545bf5a5
carlogilmar <carlogilmar12@gmail.com>
2017-11-29 15:30:45 -0600

    Adding inout for add a new skill test: PENDIENT

--- app/routes/evaluations/evaluation.js
+++ app/routes/evaluations/evaluation.js
@@ -2,7 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model:function(params){
-    //return this.store.query('skillTest', {evaluation: params.evaluation_id})
     return this.store.findRecord('evaluation', params.evaluation_id);
+  },
+  actions:{
+    addTest:function(){
+      var skillTest = this.store.createRecord('skill-test',{
+        description: this.controller.get('description')
+      });
+      alert('Agregando un test');
+    }
   }
 });
