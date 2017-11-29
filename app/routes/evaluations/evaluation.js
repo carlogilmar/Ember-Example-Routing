d3a6ac1cb9c1840734e7abf27536379ef9099802
carlogilmar <carlogilmar12@gmail.com>
2017-11-29 13:13:22 -0600

    Finding record evaluation by id

--- app/routes/evaluations/evaluation.js
+++ app/routes/evaluations/evaluation.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model:function(params){
-    return this.store.query('skillTest', {evaluation: params.evaluation_id})
+    //return this.store.query('skillTest', {evaluation: params.evaluation_id})
+    return this.store.findRecord('evaluation', params.evaluation_id);
   }
 });
