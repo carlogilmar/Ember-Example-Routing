482d114c75944a360ffed540a933bbdec300d34d
carlogilmar <carlogilmar12@gmail.com>
2017-11-29 12:38:53 -0600

    Adding fixture -evaluations- and put fake data

---
+++ mirage/fixtures/evaluations.js
@@ -0,0 +1,6 @@
+export default [
+    {id:1, title:"Evaluacion 1", author:"carlogilmar" },
+    {id:2, title:"Evaluacion 2", author:"carlogilmar" },
+    {id:3, title:"Evaluacion 3", author:"carlogilmar" },
+    {id:4, title:"Evaluacion 4", author:"carlogilmar" }
+];
